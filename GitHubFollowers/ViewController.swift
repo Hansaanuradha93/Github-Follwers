711d70fab725c62abc9cae17a54c6e9669757534
Hansa Anuradha <hansaanuradha@Hansas-MacBook-Pro.local>
2019-12-29 21:58:48 +0530

    Innitial confugurations of the iOS app

---
+++ GitHubFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHubFollowers
+//
+//  Created by Hansa Anuradha on 12/29/19.
+//  Copyright © 2019 Hansa Anuradha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
