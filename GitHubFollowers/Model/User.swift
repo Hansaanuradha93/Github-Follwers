ab129393095ca2ab9ab9fb77ef604528e0ef8c3e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-01-03 17:44:17 +0530

    Created custom alert to conntroll errors and created models

--- GitHubFollowers/Model/User.swift
+++ GitHubFollowers/Model/User.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var company: String?
+    var email: String
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
