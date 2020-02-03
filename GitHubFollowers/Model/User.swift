56979e2dfb93a092338ec357c291160219908b79
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-02-03 12:41:35 +0530

    Added User model

--- GitHubFollowers/Model/User.swift
+++ GitHubFollowers/Model/User.swift
@@ -8,18 +8,18 @@
 
 import Foundation
 
-struct User: Codable {
-    var login: String
-    var avatarUrl: String
-    var name: String?
-    var location: String?
-    var bio: String?
-    var company: String?
-    var email: String
-    var publicRepos: Int
-    var publicGists: Int
-    var htmlUrl: String
-    var following: Int
-    var followers: Int
-    var createdAt: String
+struct User: Codable, Hashable {
+    let login: String
+    let avatarUrl: String
+    let name: String
+    let location: String
+    let bio: String?
+    let company: String
+    let email: String?
+    let publicRepos: Int
+    let publicGists: Int
+    let htmlUrl: String
+    let following: Int
+    let followers: Int
+    let createdAt: String
 }
