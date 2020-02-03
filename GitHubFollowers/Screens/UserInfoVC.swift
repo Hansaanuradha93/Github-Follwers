c68824da4934584eb1d1431a7623eed2fd8493f0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-02-03 12:46:34 +0530

    Added getUserInfo method to fetch the user data

--- GitHubFollowers/Screens/UserInfoVC.swift
+++ GitHubFollowers/Screens/UserInfoVC.swift
@@ -14,14 +14,43 @@ class UserInfoVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor                = .systemBackground
+        configureViewController()
+        configureNavigationbar()
+        getUserInfo(username: username)
+    }
+    
+    @objc func dismissVC() {
+        dismiss(animated: true)
+    }
+    
+    
+    private func configureViewController() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    private func configureNavigationbar() {
         navigationItem.title                = username
         let dontButton                      = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem   = dontButton
     }
     
-    @objc func dismissVC() {
-        dismiss(animated: true)
+    
+    private func getUserInfo(username: String) {
+        self.showLoadingView()
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+        guard let self = self else { return }
+            self.dismissLoadingView()
+            
+            switch result {
+            case .success(let user):
+                print(user)
+            
+            case .failure(let error):
+                print(error)
+                self.presentGFAlertOnMainTread(title: "Bad Stuff Happened", message: error.rawValue, buttonTitle: "OK")
+            }
+        }
     }
 
 }
