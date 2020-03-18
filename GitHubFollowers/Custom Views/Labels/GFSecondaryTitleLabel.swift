e2357c8340c769a9ec97f74a6e1dc54e0cc60128
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-18 20:14:28 +0530

    GFSecondaryTitleLabel added

--- GitHubFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
+++ GitHubFollowers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -1,7 +1,7 @@
 import UIKit
 
-class GFBodyLabel: UILabel {
-    
+class GFSecondaryTitleLabel: UILabel {
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
@@ -11,18 +11,17 @@ class GFBodyLabel: UILabel {
         fatalError("init(coder:) has not been implemented")
     }
 
-    init(textAlignment: NSTextAlignment) {
+    init(fontSize: CGFloat) {
         super.init(frame: .zero)
-        self.textAlignment = textAlignment
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
         configure()
     }
     
     private func configure() {
         textColor                   = .secondaryLabel
-        font = UIFont.preferredFont(forTextStyle: .body)
         adjustsFontSizeToFitWidth   = true
-        minimumScaleFactor          = 0.75
-        lineBreakMode               = .byWordWrapping
+        minimumScaleFactor          = 0.90
+        lineBreakMode               = .byTruncatingTail
         translatesAutoresizingMaskIntoConstraints = false
     }
     
