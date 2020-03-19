d8f77050310d43dc7465d9477da1b2c3333a77b8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-19 16:27:51 +0530

    convertToDate extension added to Stirng class

--- GitHubFollowers/Extensions/String+Ext.swift
+++ GitHubFollowers/Extensions/String+Ext.swift
@@ -25,4 +25,12 @@ extension String {
     func removeWhitespaces() -> String {
         return components(separatedBy: .whitespaces).joined()
     }
+    
+    func convertToDate() -> Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = .current
+        return dateFormatter.date(from: self)
+    }
 }
