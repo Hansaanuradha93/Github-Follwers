f6059f23f5ea5733b2c99cec91c4d9226265f26c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-19 16:32:32 +0530

    convertToMonthYearFormat extension added to Date class

--- GitHubFollowers/Extensions/Date+Ext.swift
+++ GitHubFollowers/Extensions/Date+Ext.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 extension Date {
-    
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
 }
--- GitHubFollowers/Extensions/String+Ext.swift
+++ GitHubFollowers/Extensions/String+Ext.swift
@@ -27,10 +27,10 @@ extension String {
     }
     
     func convertToDate() -> Date? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
-        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone = .current
+        let dateFormatter           = DateFormatter()
+        dateFormatter.dateFormat    = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale        = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone      = .current
         return dateFormatter.date(from: self)
     }
 }
