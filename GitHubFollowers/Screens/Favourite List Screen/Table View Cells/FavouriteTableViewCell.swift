1b172a0ee8c01cc263cf0bb1c0db695e3d02566a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-21 15:27:23 +0530

    FavoriteTableViewCell designed

--- GitHubFollowers/Screens/Favourite List Screen/Table View Cells/FavouriteTableViewCell.swift
+++ GitHubFollowers/Screens/Favourite List Screen/Table View Cells/FavouriteTableViewCell.swift
@@ -2,12 +2,45 @@ import UIKit
 
 class FavouriteTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
+    static let reuseID  = "FavouriteCell"
+    let avatarImageView = GFAvatarImageView(frame: .zero)
+    let usernameLabel   = GFTitleLabel(textAlignment: .left, fontSize: 26)
+    
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configure()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    
+    func set(favourite: Follower) {
+        usernameLabel.text = favourite.login
+        avatarImageView.downloadImage(from: favourite.avatarUrl ?? "")
+    }
+    
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        accessoryType           = .disclosureIndicator
+        let padding: CGFloat    = 12
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
+            
+            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
+            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
+            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
+            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
+        ])
+    }
 }
