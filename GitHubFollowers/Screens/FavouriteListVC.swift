b4e29636ce88ae56573723784cb15c1401dcd665
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-21 14:49:27 +0530

    Retrieving favourite list working

--- GitHubFollowers/Screens/FavouriteListVC.swift
+++ GitHubFollowers/Screens/FavouriteListVC.swift
@@ -4,7 +4,14 @@ class FavouriteListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemTeal
+        PersistenceManager.retrieveFavourites { [weak self] result in
+            guard let self = self else { return }
+            switch result {
+            case .success(let favourites):
+                print("Favourites: \(favourites)")
+            case .failure(let error):
+                print("Persistence error: \(error.rawValue)")
+            }
+        }
     }
 }
