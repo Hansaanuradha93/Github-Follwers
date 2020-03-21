1ae7bd951ea46e28f4e8b9b9a4b4f8be4bbcf01c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-21 20:12:03 +0530

    Basic table view configurations done

--- GitHubFollowers/Screens/Favourite List Screen/FavouriteListVC.swift
+++ GitHubFollowers/Screens/Favourite List Screen/FavouriteListVC.swift
@@ -2,16 +2,69 @@ import UIKit
 
 class FavouriteListVC: UIViewController {
 
+    var tableView: UITableView!
+    var favourites: [Follower]          = []
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        getFavourites()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureTableView()
+    }
+    
+    
+    private func configureViewController() {
+        view.backgroundColor                = .systemBackground
+        title                               = "Favourites"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    
+    private func getFavourites() {
         PersistenceManager.retrieveFavourites { [weak self] result in
             guard let self = self else { return }
             switch result {
             case .success(let favourites):
-                print("Favourites: \(favourites)")
+                self.favourites = favourites
+                DispatchQueue.main.async { self.tableView.reloadData() }
             case .failure(let error):
                 print("Persistence error: \(error.rawValue)")
             }
         }
+    }
+    
+    
+    private func configureTableView() {
+        tableView                   = UITableView(frame: view.bounds, style: .plain)
+        view.addSubview(tableView)
+        tableView.backgroundColor   = .systemBackground
+        tableView.dataSource        = self
+        tableView.delegate          = self
+        tableView.register(FavouriteTableViewCell.self, forCellReuseIdentifier: FavouriteTableViewCell.reuseID)
+    }
+}
+
+extension FavouriteListVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favourites.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavouriteTableViewCell.reuseID, for: indexPath) as! FavouriteTableViewCell
+        cell.set(favourite: favourites[indexPath.row])
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Item selected")
     }
 }
