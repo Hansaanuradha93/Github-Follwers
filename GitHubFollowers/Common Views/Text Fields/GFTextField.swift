8d5467f7c1e527373c53424972ecd07627ba2122
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-22 10:58:47 +0530

    Clear button mode added and clened code in GFTextField

--- GitHubFollowers/Common Views/Text Fields/GFTextField.swift
+++ GitHubFollowers/Common Views/Text Fields/GFTextField.swift
@@ -7,27 +7,30 @@ class GFTextField: UITextField {
         configure()
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
     private func configure() {
         translatesAutoresizingMaskIntoConstraints = false
         
-        layer.cornerRadius  = 10
-        layer.borderWidth   = 2
-        layer.borderColor   = UIColor.systemGray4.cgColor
+        layer.cornerRadius          = 10
+        layer.borderWidth           = 2
+        layer.borderColor           = UIColor.systemGray4.cgColor
         
-        textColor           = .label
-        tintColor = .label
-        textAlignment       = .center
-        font = UIFont.preferredFont(forTextStyle: .title2)
-        adjustsFontSizeToFitWidth = true
-        minimumFontSize     = 12
+        textColor                   = .label
+        tintColor                   = .label
+        textAlignment               = .center
+        font                        = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth   = true
+        minimumFontSize             = 12
         
-        backgroundColor     = .tertiarySystemBackground
-        autocorrectionType  = .no
-        returnKeyType       = .go
-        placeholder = "Enter a username"
+        backgroundColor             = .tertiarySystemBackground
+        autocorrectionType          = .no
+        returnKeyType               = .go
+        clearButtonMode             = .whileEditing
+        placeholder                 = "Enter a username"
     }
 }
