766a5f4e0ccd3d370725856883af403e7435ecd0
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-22 12:37:07 +0530

    Custom button cleaned

--- GitHubFollowers/Common Views/Buttons/GFButton.swift
+++ GitHubFollowers/Common Views/Buttons/GFButton.swift
@@ -4,31 +4,33 @@ class GFButton: UIButton {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        // Custom code
         configure()
     }
+    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(backgroundColor: UIColor, title: String) {
-        super.init(frame: .zero)
-        self.backgroundColor    = backgroundColor
+    
+    convenience init(backgroundColor: UIColor, title: String) {
+        self.init(frame: .zero)
         self.setTitle(title, for: .normal)
-        configure()
+        self.backgroundColor    = backgroundColor
     }
+   
     
     private func configure() {
         layer.cornerRadius      = 10
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
         setTitleColor(.white, for: .normal)
-        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         translatesAutoresizingMaskIntoConstraints = false
     }
     
+    
     func setup(backgroundColor: UIColor, title: String) {
-        self.backgroundColor = backgroundColor
         self.setTitle(title, for: .normal)
+        self.backgroundColor = backgroundColor
     }
 }
 
