64946249f647ee33d7e9cfc854dd608a6ea1be44
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-22 10:36:18 +0530

    SearchVC code cleaned

--- GitHubFollowers/Screens/Search Screen/SearchVC.swift
+++ GitHubFollowers/Screens/Search Screen/SearchVC.swift
@@ -5,25 +5,20 @@ class SearchVC: UIViewController {
     let logoImageView       = UIImageView()
     let usernameTextField   = GFTextField()
     let callToActionButton  = GFButton(backgroundColor: .systemGreen, title: "Get Follwers")
-    
     var isUsernameEntered: Bool { return  !usernameTextField.text!.isEmpty }
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemBackground // Will change accroding to both dark and light modes
-        configureLogoImageView()
-        configureTextField()
-        configureCallToActionButton()
+        view.backgroundColor = .systemBackground
+        configureUIElements()
         createTapGesture()
     }
     
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        navigationController?.setNavigationBarHidden(true, animated: true) // Hide the navigation bar
-        
-        // Lets clear the Search screen everytime when the user come back 
+        navigationController?.setNavigationBarHidden(true, animated: true)
         usernameTextField.text = ""
         view.endEditing(true)
     }
@@ -34,57 +29,48 @@ class SearchVC: UIViewController {
         view.addGestureRecognizer(tap)
     }
     
+    
     @objc func pushFollwersViewController() {
-        
         guard isUsernameEntered else {
             presentGFAlertOnMainTread(title: "Empty Username", message: "Please enter a username. We need to know who to look for 😀.", buttonTitle: "Ok")
             return
         }
-        
         let follwersListVC      = FollowersListVC()
         follwersListVC.username = usernameTextField.text
         follwersListVC.title    = usernameTextField.text
         navigationController?.pushViewController(follwersListVC, animated: true)
     }
     
-    func configureLogoImageView() {
+    
+    func configureUIElements() {
         view.addSubview(logoImageView)
+        view.addSubview(usernameTextField)
+        view.addSubview(callToActionButton)
+
         logoImageView.translatesAutoresizingMaskIntoConstraints = false
         logoImageView.image = UIImage(named: "gh-logo")
         
+        usernameTextField.delegate = self
+
+        callToActionButton.addTarget(self, action: #selector(pushFollwersViewController), for: .touchUpInside)
+
         NSLayoutConstraint.activate([
             logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 80),
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
-            logoImageView.widthAnchor.constraint(equalToConstant: 200)
-        ])
-    }
-    
-    func configureTextField() {
-        view.addSubview(usernameTextField)
-        usernameTextField.delegate = self
-        
-        NSLayoutConstraint.activate([
+            logoImageView.widthAnchor.constraint(equalToConstant: 200),
+            
             usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
             usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
-            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
-        ])
-    }
-    
-    func configureCallToActionButton() {
-        view.addSubview(callToActionButton)
-        callToActionButton.addTarget(self, action: #selector(pushFollwersViewController), for: .touchUpInside)
-        
-        NSLayoutConstraint.activate([
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50),
+            
             callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
             callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             callToActionButton.heightAnchor.constraint(equalToConstant: 50)
-            
         ])
     }
-
 }
 
 
