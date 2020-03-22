05ae426d59ab31b2892fc5734447ed7d37dd4251
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-22 10:17:48 +0530

    GFTabBar implemented

--- GitHubFollowers/Common Views/Tab Bar/GFTabBar.swift
+++ GitHubFollowers/Common Views/Tab Bar/GFTabBar.swift
@@ -2,8 +2,28 @@ import UIKit
 
 class GFTabBar: UITabBarController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UITabBar.appearance().tintColor = .systemGreen
+        viewControllers = [createSearchNC(), createFavouriteListNC()]
     }
 
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC            = SearchVC()
+        searchVC.title          = "Search"
+        searchVC.tabBarItem     = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    
+    func createFavouriteListNC() -> UINavigationController {
+        let favouriteListVC             = FavouriteListVC()
+        favouriteListVC.title           = "Favourites"
+        favouriteListVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favouriteListVC)
+    }
 }
--- GitHubFollowers/SceneDelegate.swift
+++ GitHubFollowers/SceneDelegate.swift
@@ -6,58 +6,30 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        guard let windowScene = (scene as? UIWindowScene) else { return } // Create window scenne
-        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = createTabBarController()
+        window?.rootViewController = GFTabBar()
         window?.makeKeyAndVisible()
         configureNavigationBar()
+    }
+    
+    
+    func configureNavigationBar() { UINavigationBar.appearance().tintColor = .systemGreen }
 
-    }
     
-    // Create Search Navigation Controller
-    func createSearchNC() -> UINavigationController {
-        let searchVC            = SearchVC()
-        searchVC.title          = "Search"
-        searchVC.tabBarItem     = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        
-        return UINavigationController(rootViewController: searchVC)
-    }
-    
-    
-    // Create Favourite List Navigation Controller
-    func createFavouriteListNC() -> UINavigationController {
-        let favouriteListVC             = FavouriteListVC()
-        favouriteListVC.title           = "Favourites"
-        favouriteListVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        
-        return UINavigationController(rootViewController: favouriteListVC)
-    }
-    
-    
-    // Create Tab Bar Controller
-    func createTabBarController() -> UITabBarController {
-        // Create tab bar controller
-        let tabBar = UITabBarController()
-        UITabBar.appearance().tintColor = .systemGreen
-        tabBar.viewControllers = [createSearchNC(), createFavouriteListNC()] // Add view controllers to the tab bar
-        return tabBar
-    }
-    
-    func configureNavigationBar() {
-        UINavigationBar.appearance().tintColor = .systemGreen
-    }
-
     func sceneDidDisconnect(_ scene: UIScene) { }
 
+    
     func sceneDidBecomeActive(_ scene: UIScene) { }
 
+    
     func sceneWillResignActive(_ scene: UIScene) { }
 
+    
     func sceneWillEnterForeground(_ scene: UIScene) { }
 
+    
     func sceneDidEnterBackground(_ scene: UIScene) { }
 }
 
