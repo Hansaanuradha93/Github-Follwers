3bbf2b33eea5ab820ea9565cd5358411c88e73ad
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-22 10:54:53 +0530

    GFError cleaned

--- GitHubFollowers/Utilities/ErrorMessages.swift
+++ GitHubFollowers/Utilities/ErrorMessages.swift
@@ -1,12 +1,10 @@
 import Foundation
 
 enum GFError: String, Error {
-    
     case invalidUsername        = "This username created an invalid request. Please try again."
     case unableToComplete       = "Unable to complete the request. Please check your internet connection."
     case invalidResponse        = "Invalid response from the server. Please try again"
     case invalidData            = "Data recieved from the server is invalid. Please try again."
     case unableToFavourute      = "There was an error favouriting this user. Please try again."
     case alreadyInFavourites    = "This user is already in your favourite list."
-    
 }
