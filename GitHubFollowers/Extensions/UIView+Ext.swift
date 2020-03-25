f1493e157241a8b6b4ffc262336077a3ea9d6a5e
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2020-03-25 09:46:07 +0530

    Added pinToEdges() extension

--- GitHubFollowers/Extensions/UIView+Ext.swift
+++ GitHubFollowers/Extensions/UIView+Ext.swift
@@ -2,6 +2,17 @@ import UIKit
 
 extension UIView {
     
+    func pinToEdges(of superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            topAnchor.constraint(equalTo: superView.topAnchor),
+            leadingAnchor.constraint(equalTo: superView.leadingAnchor),
+            trailingAnchor.constraint(equalTo: superView.trailingAnchor),
+            bottomAnchor.constraint(equalTo: superView.bottomAnchor)
+        ])
+    }
+    
+    
     func addSubviews(_ views: UIView...) {
         for view in views {addSubview(view) }
     }
