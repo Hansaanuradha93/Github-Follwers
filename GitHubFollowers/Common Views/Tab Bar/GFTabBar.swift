efb59ceab67468c2a8c6973fd23c7105641415cd
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-05 22:30:30 +0530

    GFTabBar code style maintained

--- GitHubFollowers/Common Views/Tab Bar/GFTabBar.swift
+++ GitHubFollowers/Common Views/Tab Bar/GFTabBar.swift
@@ -2,11 +2,11 @@ import UIKit
 
 class GFTabBar: UITabBarController {
 
-    // MARK: - View Controller
+    // MARK: View Controller
     override func viewDidLoad() {
         super.viewDidLoad()
         UITabBar.appearance().tintColor = .systemGreen
-        viewControllers                 = [createSearchNC(), createFavouriteListNC()]
+        viewControllers = [createSearchNC(), createFavouriteListNC()]
     }
 }
 
@@ -15,18 +15,18 @@ class GFTabBar: UITabBarController {
 extension GFTabBar {
     
     private func createSearchNC() -> UINavigationController {
-        let searchVC            = SearchVC()
-        searchVC.title          = "Search"
-        searchVC.tabBarItem     = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
         
         return UINavigationController(rootViewController: searchVC)
     }
     
     
     private func createFavouriteListNC() -> UINavigationController {
-        let favouriteListVC             = FavouriteListVC()
-        favouriteListVC.title           = "Favourites"
-        favouriteListVC.tabBarItem      = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        let favouriteListVC = FavouriteListVC()
+        favouriteListVC.title = "Favourites"
+        favouriteListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
         
         return UINavigationController(rootViewController: favouriteListVC)
     }
