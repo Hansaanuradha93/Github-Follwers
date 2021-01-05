923f9a3d6b17e60b6b663d57ea2ca9a9b7b03d54
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-05 22:33:16 +0530

    GFAlertContainerView code style maintained

--- GitHubFollowers/Common Views/Views/GFAlertContainerView.swift
+++ GitHubFollowers/Common Views/Views/GFAlertContainerView.swift
@@ -2,16 +2,14 @@ import UIKit
 
 class GFAlertContainerView: UIView {
 
-    // MARK: - Initializers
+    // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { fatalError() }
 }
 
 
@@ -20,10 +18,9 @@ extension GFAlertContainerView {
     
     private func configure() {
         translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor     = .systemBackground
-        layer.cornerRadius  = 16
-        layer.borderWidth   = 2
-        layer.borderColor   = UIColor.white.cgColor
-        
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
     }
 }
