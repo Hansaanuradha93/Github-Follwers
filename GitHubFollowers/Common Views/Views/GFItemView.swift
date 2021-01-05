18593c3e4e63e743da3a112de0b84759c1078fd8
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-05 22:35:54 +0530

    GFItemView code refactored

--- GitHubFollowers/Common Views/Views/GFItemView.swift
+++ GitHubFollowers/Common Views/Views/GFItemView.swift
@@ -1,53 +1,49 @@
 import UIKit
 
-// MARK: - Enums
+// MARK: Enums
 enum ItemInfoType {
-    
     case repos, gists, followers, following
 }
 
 
 class GFItemView: UIView {
     
-    // MARK: - Properties
+    // MARK: Properties
     private let symbolImageView = UIImageView()
-    private let titleLabel      = GFTitleLabel(textAlignment: .left, fontSize: 14)
-    private let countLabel      = GFTitleLabel(textAlignment: .center, fontSize: 14)
+    private let titleLabel = GFTitleLabel(textAlignment: .left, fontSize: 14)
+    private let countLabel = GFTitleLabel(textAlignment: .center, fontSize: 14)
     
     
-    // MARK: - Initializers
+    // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    } 
+    required init?(coder: NSCoder) { fatalError() }
 }
 
 
 // MARK: - Private Methods
-
 extension GFItemView {
     
     func setup(itemInfoType: ItemInfoType, withCount count: Int) {
         switch itemInfoType {
         case .repos:
-            symbolImageView.image   = SFSymbols.repos
-            titleLabel.text         = "Public Repos"
+            symbolImageView.image = SFSymbols.repos
+            titleLabel.text = "Public Repos"
         case .gists:
-            symbolImageView.image   = SFSymbols.gists
-            titleLabel.text         = "Public Gists"
+            symbolImageView.image = SFSymbols.gists
+            titleLabel.text = "Public Gists"
         case .followers:
-            symbolImageView.image   = SFSymbols.followers
-            titleLabel.text         = "Followers"
+            symbolImageView.image = SFSymbols.followers
+            titleLabel.text = "Followers"
         case .following:
-            symbolImageView.image   = SFSymbols.followings
-            titleLabel.text         = "Following"
+            symbolImageView.image = SFSymbols.followings
+            titleLabel.text = "Following"
         }
-        countLabel.text             = "\(count)"
+        countLabel.text = "\(count)"
     }
     
     
@@ -56,7 +52,7 @@ extension GFItemView {
         
         symbolImageView.translatesAutoresizingMaskIntoConstraints = false
         symbolImageView.contentMode = .scaleAspectFill
-        symbolImageView.tintColor   = .label
+        symbolImageView.tintColor = .label
         
         NSLayoutConstraint.activate([
             symbolImageView.topAnchor.constraint(equalTo: self.topAnchor),
