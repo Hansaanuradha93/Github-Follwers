23a3aaef73b5e432666fc4db13df95e064504042
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-06 18:08:21 +0530

    GFButton code refactored

--- GitHubFollowers/Common Views/Buttons/GFButton.swift
+++ GitHubFollowers/Common Views/Buttons/GFButton.swift
@@ -2,16 +2,14 @@ import UIKit
 
 class GFButton: UIButton {
 
-    // MARK: - Initializers
+    // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { fatalError() }
     
     
     convenience init(backgroundColor: UIColor, title: String) {
@@ -32,8 +30,8 @@ extension GFButton {
     
     
     private func configure() {
-        layer.cornerRadius      = 10
-        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        layer.cornerRadius = 10
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         setTitleColor(.white, for: .normal)
         translatesAutoresizingMaskIntoConstraints = false
     }
