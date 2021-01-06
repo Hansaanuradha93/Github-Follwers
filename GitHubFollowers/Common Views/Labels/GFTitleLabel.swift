b32772e41c40e5c3b4ea8df8060b5ce0ecc3416d
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-06 18:07:08 +0530

    GFTitleLabel code refactored

--- GitHubFollowers/Common Views/Labels/GFTitleLabel.swift
+++ GitHubFollowers/Common Views/Labels/GFTitleLabel.swift
@@ -2,22 +2,20 @@ import UIKit
 
 class GFTitleLabel: UILabel {
 
-    // MARK: - Initializers
+    // MARK: Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+    required init?(coder: NSCoder) { fatalError() }
 
     
     convenience init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
         self.init(frame: .zero)
-        self.textAlignment  = textAlignment
-        self.font           = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
     }
 }
 
@@ -26,10 +24,10 @@ class GFTitleLabel: UILabel {
 extension GFTitleLabel {
     
     private func configure() {
-        textColor                   = .label
-        adjustsFontSizeToFitWidth   = true
-        minimumScaleFactor          = 0.9
-        lineBreakMode               = .byTruncatingTail
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
         translatesAutoresizingMaskIntoConstraints = false
     }
 }
