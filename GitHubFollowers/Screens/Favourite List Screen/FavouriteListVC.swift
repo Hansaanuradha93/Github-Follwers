3f090c60b05e9e8f808c5686773a689e5b07f7fc
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-09 06:08:11 +0530

    FavouriteListVC code refactored

--- GitHubFollowers/Screens/Favourite List Screen/FavouriteListVC.swift
+++ GitHubFollowers/Screens/Favourite List Screen/FavouriteListVC.swift
@@ -2,14 +2,14 @@ import UIKit
 
 class FavouriteListVC: DataLoadingVC {
 
-    // MARK: - Properties
+    // MARK: Properties
     private var tableView: UITableView!
     private var dataSource: FavouriteDataSource!
-    var delegate: FavouriteDelegate!
     private var favourites: [Follower] = []
+    var delegate: FavouriteDelegate!
 
     
-    // MARK: - View Controller
+    // MARK: View Controller
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         getFavourites()
@@ -25,16 +25,16 @@ class FavouriteListVC: DataLoadingVC {
 
 
 // MARK: - Private Methods
-extension FavouriteListVC {
+private extension FavouriteListVC {
     
-    private func configureViewController() {
-        view.backgroundColor                                    = .systemBackground
-        title                                                   = "Favourites"
-        navigationController?.navigationBar.prefersLargeTitles  = true
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
+        title = "Favourites"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     
-    private func getFavourites() {
+    func getFavourites() {
         PersistenceManager.retrieveFavourites { [weak self] result in
             guard let self = self else { return }
             switch result {
@@ -47,7 +47,7 @@ extension FavouriteListVC {
     }
     
     
-    private func updateUI(withFavourites favourites: [Follower]) {
+    func updateUI(withFavourites favourites: [Follower]) {
         if favourites.isEmpty {
             let message = "No favourites?\nGo follow a user from follower screen 😀"
             DispatchQueue.main.async { self.showEmptyStateView(with: message, in: self.view) }
@@ -62,20 +62,20 @@ extension FavouriteListVC {
     }
     
     
-    private func configureTableView() {
-        tableView                   = UITableView(frame: view.bounds, style: .plain)
-        tableView.backgroundColor   = .systemBackground
-        tableView.separatorStyle    = .none
+    func configureTableView() {
+        tableView = UITableView(frame: view.bounds, style: .plain)
+        tableView.backgroundColor = .systemBackground
+        tableView.separatorStyle = .none
         view.addSubview(tableView)
         tableView.removeExcessCells()
         tableView.register(FavouriteTableViewCell.self, forCellReuseIdentifier: FavouriteTableViewCell.reuseID)
     }
     
     
-    private func setDataSourceAndDelegate(with favourites: [Follower]) {
-        dataSource             = FavouriteDataSource(favourites: favourites, viewController: self)
-        delegate               = FavouriteDelegate(favourites: favourites, navigationController: self.navigationController)
-        tableView.dataSource   = dataSource
-        tableView.delegate     = delegate
+    func setDataSourceAndDelegate(with favourites: [Follower]) {
+        dataSource = FavouriteDataSource(favourites: favourites, viewController: self)
+        delegate = FavouriteDelegate(favourites: favourites, navigationController: self.navigationController)
+        tableView.dataSource = dataSource
+        tableView.delegate = delegate
     }
 }
