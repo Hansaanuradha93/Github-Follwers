5f72c394e2947e90040f795efbe77f71759ba8e9
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-12 19:32:13 +0530

    UIViewController code style maintained

--- GitHubFollowers/Extensions/UIViewController+Ext.swift
+++ GitHubFollowers/Extensions/UIViewController+Ext.swift
@@ -5,17 +5,17 @@ extension UIViewController {
     
     func presentGFAlertOnMainTread(title: String, message: String, buttonTitle: String) {
         DispatchQueue.main.async {
-            let alertVC                     = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
-            alertVC.modalPresentationStyle  = .overFullScreen
-            alertVC.modalTransitionStyle    = .crossDissolve
+            let alertVC = GFAlertVC(title: title, message: message, buttonTitle: buttonTitle)
+            alertVC.modalPresentationStyle = .overFullScreen
+            alertVC.modalTransitionStyle = .crossDissolve
             self.present(alertVC, animated: true, completion: nil)
         }
     }
     
     
     func presentSafariVC(with url: URL) {
-        let safariVC                        = SFSafariViewController(url: url)
-        safariVC.preferredControlTintColor  = .systemGreen
+        let safariVC = SFSafariViewController(url: url)
+        safariVC.preferredControlTintColor = .systemGreen
         present(safariVC, animated: true)
     }
 }
