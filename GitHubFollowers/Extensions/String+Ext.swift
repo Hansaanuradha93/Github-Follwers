bf3a03945c711ce2af0388476e85c1db2a026656
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-12 19:33:37 +0530

    String extensions code style maintained

--- GitHubFollowers/Extensions/String+Ext.swift
+++ GitHubFollowers/Extensions/String+Ext.swift
@@ -3,22 +3,22 @@ import UIKit
 extension String {
     
     var isValidEmail: Bool {
-        let emailFormat         = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
-        let emailPredicate      = NSPredicate(format: "SELF MATCHES %@", emailFormat)
+        let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format: "SELF MATCHES %@", emailFormat)
         return emailPredicate.evaluate(with: self)
     }
 
 
     var isValidPassword: Bool {
-        let passwordFormat      = "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}"
-        let passwordPredicate   = NSPredicate(format: "SELF MATCHES %@", passwordFormat)
+        let passwordFormat = "(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}"
+        let passwordPredicate = NSPredicate(format: "SELF MATCHES %@", passwordFormat)
         return passwordPredicate.evaluate(with: self)
     }
 
 
     var isValidPhoneNumber: Bool {
-        let phoneNumberFormat   = "^\\d{3}-\\d{3}-\\d{4}$"
-        let numberPredicate     = NSPredicate(format: "SELF MATCHES %@", phoneNumberFormat)
+        let phoneNumberFormat = "^\\d{3}-\\d{3}-\\d{4}$"
+        let numberPredicate = NSPredicate(format: "SELF MATCHES %@", phoneNumberFormat)
         return numberPredicate.evaluate(with: self)
     }
 
@@ -29,10 +29,10 @@ extension String {
     
     
     func convertToDate() -> Date? {
-        let dateFormatter           = DateFormatter()
-        dateFormatter.dateFormat    = "yyyy-MM-dd'T'HH:mm:ssZ"
-        dateFormatter.locale        = Locale(identifier: "en_US_POSIX")
-        dateFormatter.timeZone      = .current
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.timeZone = .current
         return dateFormatter.date(from: self)
     }
     
