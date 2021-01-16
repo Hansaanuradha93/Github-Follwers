47d0e1003cec3dd9f17fcfeba4c6b1502c7808e6
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-16 23:11:53 +0530

    FavouriteTableViewCell ui code refactored

--- GitHubFollowers/Screens/Favourite List Screen/Table View Cells/FavouriteTableViewCell.swift
+++ GitHubFollowers/Screens/Favourite List Screen/Table View Cells/FavouriteTableViewCell.swift
@@ -34,16 +34,9 @@ extension FavouriteTableViewCell {
         accessoryType = .disclosureIndicator
         let padding: CGFloat = 12
         
-        NSLayoutConstraint.activate([
-            avatarImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            avatarImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: padding),
-            avatarImageView.widthAnchor.constraint(equalToConstant: 60),
-            avatarImageView.heightAnchor.constraint(equalToConstant: 60),
-            
-            usernameLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            usernameLabel.leadingAnchor.constraint(equalTo: avatarImageView.trailingAnchor, constant: 24),
-            usernameLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -padding),
-            usernameLabel.heightAnchor.constraint(equalToConstant: 40)
-        ])
+        avatarImageView.anchor(top: nil, leading: contentView.leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: 0, left: padding, bottom: 0, right: 0), size: .init(width: 60, height: 60))
+        avatarImageView.centerVerticallyInSuperView()
+        usernameLabel.anchor(top: nil, leading: avatarImageView.trailingAnchor, bottom: nil, trailing: contentView.trailingAnchor, padding: .init(top: 0, left: 2 * padding, bottom: 0, right: padding))
+        usernameLabel.centerVerticallyInSuperView()
     }
 }
