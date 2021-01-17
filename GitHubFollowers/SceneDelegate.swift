9776f871f351755b0693258a17f53f96b155352a
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-17 20:38:27 +0530

    SceneDelegate code cleaned

--- GitHubFollowers/SceneDelegate.swift
+++ GitHubFollowers/SceneDelegate.swift
@@ -13,23 +13,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.makeKeyAndVisible()
         configureNavigationBar()
     }
-    
+}
+
+
+// MARK: - Private Methods
+private extension SceneDelegate {
     
     func configureNavigationBar() { UINavigationBar.appearance().tintColor = .systemGreen }
-
-    
-    func sceneDidDisconnect(_ scene: UIScene) { }
-
-    
-    func sceneDidBecomeActive(_ scene: UIScene) { }
-
-    
-    func sceneWillResignActive(_ scene: UIScene) { }
-
-    
-    func sceneWillEnterForeground(_ scene: UIScene) { }
-
-    
-    func sceneDidEnterBackground(_ scene: UIScene) { }
 }
 
