31f59571b70df1c9cac14f4093526a87768a0558
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-21 22:03:03 +0530

    GFAvatarImageView code refactored

--- GitHubFollowers/Common Views/Image Views/GFAvatarImageView.swift
+++ GitHubFollowers/Common Views/Image Views/GFAvatarImageView.swift
@@ -24,12 +24,11 @@ extension GFAvatarImageView {
         layer.cornerRadius = 10
         clipsToBounds = true
         image = placeholderImage
-        translatesAutoresizingMaskIntoConstraints = false
     }
     
     func downloadImage(fromUrl url: String) {
         NetworkManager.shared.downloadImage(from: url) { [weak self] image in
-            guard let self          = self else { return }
+            guard let self = self else { return }
             DispatchQueue.main.async { self.image = image }
         }
     }
