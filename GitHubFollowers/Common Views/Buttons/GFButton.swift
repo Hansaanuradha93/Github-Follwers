2095362b89f5b384a5ce09208a5715287fcabc2c
Hansa Wickramanayake <Hansaanuradha93@gmail.com>
2021-01-22 21:32:30 +0530

    GFButton code refactored

--- GitHubFollowers/Common Views/Buttons/GFButton.swift
+++ GitHubFollowers/Common Views/Buttons/GFButton.swift
@@ -33,7 +33,7 @@ extension GFButton {
         layer.cornerRadius = 10
         titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
         setTitleColor(.white, for: .normal)
-        translatesAutoresizingMaskIntoConstraints = false
+//        translatesAutoresizingMaskIntoConstraints = false
     }
 }
 
